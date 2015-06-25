bb86a8c636043e89927cde308b3a0cfa6257686a
Jordan Walker <jordan.walker@crowdsource.com>
2015-06-24 23:14:18 -0500

    modification of metadata.rb

--- mongodb-masonr/metadata.rb
+++ mongodb-masonr/metadata.rb
@@ -1,5 +1,5 @@
-name        "mongodb-mysite"
-description 'Example cookbook for MongoDB Berkshelf based install'
+name        "mongodb-masonr"
+description 'Base cookbook for MasonrMongoDB Berkshelf based install'
 maintainer  "MyOrg"
 license     "Apache 2.0"
 version     "1.0.0"
